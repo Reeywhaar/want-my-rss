3e3fbba0d1079497291305fe8ea3fec24d5a67d6
Vyrtsev Mikhail <reeywhaar@gmail.com>
2018-11-17 15:12:09 +0300

    filter feeds without href

--- ext/page.js
+++ ext/page.js
@@ -4,6 +4,7 @@
 	)
 		.filter(el => {
 			if (!el.hasAttribute("type")) return false;
+			if (!el.hasAttribute("href")) return false;
 			const type = el.getAttribute("type");
 			if (type.indexOf("rss") !== -1 || type.indexOf("atom") !== -1)
 				return true;
