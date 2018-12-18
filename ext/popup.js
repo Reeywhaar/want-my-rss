20a97fbccd64038aa71558426be606ffee3ec8c4
Vyrtsev Mikhail <reeywhaar@gmail.com>
2018-12-18 08:08:55 +0300

    show original url in popup #8

--- ext/popup.js
+++ ext/popup.js
@@ -10,23 +10,29 @@ const types = {
 	"application/json": "json",
 };
 
+document.addEventListener("click", e => {
+	if (!(e.which === 1 || e.which === 2)) return;
+	e.preventDefault();
+	if (e.target.dataset.url) {
+		browser.runtime.sendMessage({
+			action: "open-tab",
+			url: browser.runtime.getURL(e.target.dataset.url),
+			newTab: e.which === 2 || e.metaKey,
+		});
+	}
+});
+
 const elements = links.map(el => {
 	const content = template.content.cloneNode(true);
 	const link = content.querySelector(".items__item-link");
-	link.href = browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`);
+	const extURL = browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`);
+	link.href = el.url;
+	link.dataset.url = extURL;
 	link.innerHTML =
 		(el.title || el.url) +
 		(types[el.type]
 			? ` <span style="opacity:0.6;">(${types[el.type]})</span>`
 			: "");
-	link.addEventListener("click", e => {
-		e.preventDefault();
-		browser.runtime.sendMessage({
-			action: "open-tab",
-			url: browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`),
-			newTab: e.metaKey,
-		});
-	});
 	return content;
 });
 
