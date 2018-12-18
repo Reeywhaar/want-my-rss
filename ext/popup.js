42566ef5d285dfacb44e35e00ae9ec7893d48eb9
Vyrtsev Mikhail <reeywhaar@gmail.com>
2018-12-18 08:26:17 +0300

    0.8
    * show original url in extension's popup
    * loose xml parsing from first occurance of `<?xml`

--- ext/popup.js
+++ ext/popup.js
@@ -10,23 +10,37 @@ const types = {
 	"application/json": "json",
 };
 
+function findParent(el, selector) {
+	while (!el.matches(selector)) {
+		if (el === document.body) return null;
+		el = el.parentElement;
+	}
+	return el;
+}
+
+document.addEventListener("click", e => {
+	if (!(e.which === 1 || e.which === 2)) return;
+	const el = findParent(e.target, ".items__item-link");
+	if (!el) return;
+	e.preventDefault();
+	browser.runtime.sendMessage({
+		action: "open-tab",
+		url: browser.runtime.getURL(el.dataset.url),
+		newTab: e.which === 2 || e.metaKey,
+	});
+});
+
 const elements = links.map(el => {
 	const content = template.content.cloneNode(true);
 	const link = content.querySelector(".items__item-link");
-	link.href = browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`);
+	const extURL = browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`);
+	link.href = el.url;
+	link.dataset.url = extURL;
 	link.innerHTML =
 		(el.title || el.url) +
 		(types[el.type]
 			? ` <span style="opacity:0.6;">(${types[el.type]})</span>`
 			: "");
-	link.addEventListener("click", e => {
-		e.preventDefault();
-		browser.runtime.sendMessage({
-			action: "open-tab",
-			url: browser.runtime.getURL(`show.html?url=${encodeURI(el.url)}`),
-			newTab: e.metaKey,
-		});
-	});
 	return content;
 });
 
